80f328c8cd1e8a869addf3e9b4daf910c15f9ab1
yareach8345 <mine8345@naver.com>
2025-05-24 23:28:24 +0900

    add: 라이브러리 추가
    - tailwindcss 라이브러리 의존성 추가
    - tone.js 라이브러리 의존성 추가
    
    - test목적으로 App.vue에 코드 좀 추가

--- app.vue
+++ app.vue
@@ -1,6 +1,46 @@
+<script lang="ts" setup>
+import * as Tone from 'tone'
+
+
+let synth: Tone.Synth | Tone.AMSynth | Tone.FMSynth | Tone.DuoSynth | Tone.MonoSynth | Tone.MembraneSynth | Tone.MetalSynth | Tone.PluckSynth | Tone.PolySynth
+if (import.meta.client) {
+  synth = new Tone.FMSynth().toDestination()
+}
+
+const play = async () => {
+  await Tone.start()
+  console.log('play')
+  const now = Tone.now()
+  synth.triggerAttackRelease('A5', '8n', now)
+  synth.triggerAttackRelease('A5', '8n', now + 0.3)
+  synth.triggerAttackRelease('A5', '8n', now + 0.4)
+  synth.triggerAttackRelease('C6', '8n', now + 0.5)
+  synth.triggerAttackRelease('B5', '8n', now + 0.6)
+  synth.triggerAttackRelease('A5', '8n', now + 0.7)
+  synth.triggerAttackRelease('E6', '8n', now + 0.8)
+  synth.triggerAttackRelease('E5', '8n', now + 1.1)
+  synth.triggerAttackRelease('E5', '8n', now + 1.2)
+  synth.triggerAttackRelease('G#5', '8n', now + 1.3)
+  synth.triggerAttackRelease('F#5', '8n', now + 1.4)
+  synth.triggerAttackRelease('E5', '8n', now + 1.5)
+  synth.triggerAttackRelease('A5', '8n', now + 1.6)
+  synth.triggerAttackRelease('A5', '8n', now + 1.9)
+  synth.triggerAttackRelease('A5', '8n', now + 2.0)
+  synth.triggerAttackRelease('C6', '8n', now + 2.1)
+  synth.triggerAttackRelease('B5', '8n', now + 2.2)
+  synth.triggerAttackRelease('A5', '8n', now + 2.3)
+  synth.triggerAttackRelease('E6', '8n', now + 2.4)
+  synth.triggerAttackRelease('E5', '8n', now + 2.8)
+}
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button
+      @click="play"
+  >
+    play!
+  </button>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+        tailwindcss()
+    ]
+  }
 })
