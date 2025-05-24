17146b40de91c0eef68162fd3315889a5f18437c
yareach8345 <mine8345@naver.com>
2025-05-25 00:13:39 +0900

    add: storybook 의존성 추가 & 초기화

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook-vue/nuxt';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../components/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook"
+  ],
+  "framework": {
+    "name": "@storybook-vue/nuxt",
+    "options": {}
+  }
+};
+export default config;
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,6 @@ export default defineNuxtConfig({
     plugins: [
         tailwindcss()
     ]
-  }
+  },
+  modules: ['@nuxtjs/storybook'],
 })
