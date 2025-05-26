7da3621adc58413a9169ac0a96424201bb08875a
yareach8345 <mine8345@naver.com>
2025-05-26 18:46:39 +0900

    feat: tailwind를 @nuxt/tailwind로 변경
    - nuxt에 좀더 맞게 설계된 @nuxt/tailwind로 변경

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,8 @@
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [
-        tailwindcss()
-    ]
-  },
-  modules: ['@nuxtjs/storybook'],
+  modules: [
+      '@nuxtjs/storybook',
+      '@nuxtjs/tailwindcss'
+  ],
 })
