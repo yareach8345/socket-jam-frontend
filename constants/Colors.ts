062bb8b77cb1777a7aec0b9cdb8b4f855f450dfe
yareach8345 <mine8345@naver.com>
2025-05-29 11:36:58 +0900

    feat: 설정 가능한 색상들을 담아놓은 리스트 Colors를 추가

---
+++ constants/Colors.ts
@@ -0,0 +1,8 @@
+import type { Color } from '@/types/Colors'
+
+const colors: Color[] = [
+    'red', 'orange', 'amber', 'yellow', 'lime',
+    'green', 'emerald', 'teal', 'cyan', 'sky',
+    'blue', 'indigo', 'violet', 'purple', 'fuchsia',
+    'pink', 'rose'
+]
