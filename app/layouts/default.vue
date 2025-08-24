f8b3e30c66ba460548c20dc6dc85f29171a3bb26
yareach8345 <mine8345@naver.com>
2025-08-24 11:29:36 +0900

    feature - 메인 페이지 작성

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,15 +4,11 @@
 
 <template>
   <div class="flex flex-col items-center justify-between min-h-screen bg-[url(/image/background/dark-blue.png)] bg-cover bg-center">
-    <header>
-      HEADER
-    </header>
-    <main class="bg-gray-400 bg-opacity-20 rounded-md border-white/20 backdrop-blur-md">
+    <header></header>
+    <main class="bg-gray-400 bg-opacity-20 rounded-md shadow-sm border-white/40 backdrop-blur-md">
       <slot/>
     </main>
-    <footer>
-      FOOTER
-    </footer>
+    <footer></footer>
   </div>
 </template>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,46 @@
 <script setup lang="ts">
+import { fromEvent } from 'rxjs'
 
+const userName = ref('')
+const roomNumber = ref('')
+
+const joinRoom = () => {
+  console.log(userName.value, roomNumber.value)
+}
 </script>
 
 <template>
-  <div class="text-primary font-bold underline p-52">Hello, Tailwind CSS</div>
+  <div class="p-4 flex flex-col justify-center items-center gap-3">
+    <div>
+      <h2 class="font-bold text-2xl">Welcome</h2>
+      <p class="text-lg">Please enter your <b>username</b> and <b>room number</b></p>
+    </div>
+    <form
+        @submit.prevent="joinRoom"
+        class="flex flex-col gap-3 items-center"
+    >
+      <label>
+        Username<br>
+        <input
+            class="text-lg p-1 rounded-md"
+            type="text" placeholder="Username"
+            required
+            autofocus
+            v-model="userName"
+        />
+      </label>
+      <label>
+        Room number<br>
+        <input
+            class="text-lg p-1 rounded-md"
+            type="text" placeholder="Room number"
+            required
+            v-model="roomNumber"
+        />
+      </label>
+      <button class="bg-blue-500 hover:bg-blue-700 transition-colors text-white py-1 px-2 rounded-md">Join</button>
+    </form>
+  </div>
 </template>
 
 <style scoped>
